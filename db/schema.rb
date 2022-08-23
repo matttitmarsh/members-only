3f4a9d33423bb663247eb105817d8cc41cafaaf7
Matt <matt.titmarsh@gmail.com>
2022-08-23 22:35:59 +1000

    add index to username

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_23_122051) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_23_123242) do
   create_table "posts", force: :cascade do |t|
+    t.string "title"
     t.text "body"
     t.integer "user_id", null: false
     t.datetime "created_at", null: false
@@ -30,6 +31,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_23_122051) do
     t.datetime "remember_created_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "posts", "users"
